01b50fcee70872b680e3563bfb1c49c48bfe3732
Chavannes-de-Bogis <tarek.schuetz@stud.kbw.ch>
2023-03-31 14:37:32 +0200

    Router und Grundgerüst für Seiten hinzugefügt

--- versicherungsrechner/src/App.js
+++ versicherungsrechner/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import NoPage from "./pages/NoPage";
+import Quadratmeter from "./pages/Quadratmeter";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NoPage />} />
+          <Route path="Quadratmeter" element={<Quadratmeter />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
---
+++ versicherungsrechner/src/pages/Quadratmeter.js
@@ -0,0 +1,5 @@
+const Quadratmeter= () => {
+    return <h1>Seite für Quadratmetermethode</h1>
+};
+
+export default Quadratmeter;
