9456fb2d6635ff2ab869133f25d20627570f36dc
Chavannes-de-Bogis <tarek.schuetz@stud.kbw.ch>
2023-05-12 08:46:50 +0200

    navbar entfert, weil überflüssig

--- versicherungsrechner/src/App.js
+++ versicherungsrechner/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import NoPage from "./pages/NoPage";
 import Quadratmeter from "./pages/Quadratmeter";
@@ -15,13 +14,11 @@ export default function App() {
     <div className="homediv">
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="*" element={<NoPage />} />
           <Route path="Quadratmeter" element={<Quadratmeter />} />
           <Route path="Tesdpage" element={<Tesdpage />} />
           <Route path="Summe" element={<Summe />} />
-        </Route>
       </Routes>
     </BrowserRouter>
     </div>
